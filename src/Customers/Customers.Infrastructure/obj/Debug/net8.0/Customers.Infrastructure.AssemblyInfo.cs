7a485e43929c03a9dab1dfcf87c8146b2d42ec7d
CharukaR <CharukaR@99x.io>
2025-02-26 12:14:58 +0530

    Add logs

--- src/Customers/Customers.Infrastructure/obj/Debug/net8.0/Customers.Infrastructure.AssemblyInfo.cs
+++ src/Customers/Customers.Infrastructure/obj/Debug/net8.0/Customers.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Customers.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a654320ec932edb4b63c7191a568ac5aa7677b51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Customers.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Customers.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
